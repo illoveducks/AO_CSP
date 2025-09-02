0e6dd6eb367c73510cb8f588ffebd948cba4f183
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-02 13:45:41 -0600

    variables

--- practices/variable_practice.py
+++ practices/variable_practice.py
@@ -1,5 +1,13 @@
 # AO 6th Variable practice
-
-color = blue 
-
+name = "Airam"
 number = 5
+num =565
+breakfast ="nothing "
+color = "purple"
+school = "ucas"
+year = 2025
+eye_color ="brown, really dark"
+age = 14
+Sub = "computer science"
+
+print(name, number, num, breakfast, color, school, year, eye_color, age, Sub)
