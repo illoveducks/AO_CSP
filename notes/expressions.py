2419bb135479d9b44396c41a0c9e87c9b365f42c
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-04 13:43:17 -0600

    wrote expressions

---
+++ notes/expressions.py
@@ -0,0 +1,9 @@
+# AO 6th Expression notes
+
+age = 14
+age2 = 15
+age3 = 14
+age4 = 15
+total = age + age2 + age3 + age4
+avg = total/4 
+print(age, "+", age2, "+", age3, "+", age4, "=", total, "the average would be",avg  )
