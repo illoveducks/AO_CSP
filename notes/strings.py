63b62e31281b0d13105c19af826c4cf74cc90323
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-08 13:45:31 -0600

    added code

--- notes/strings.py
+++ notes/strings.py
@@ -1,3 +1,13 @@
 # AO 6th String notes
 
 print("I did it!")
+
+first_name = input("what is your name:\n").strip().upper().title()
+
+last_name = input("what is your last name:\n")
+
+full_name = first_name + last_name
+
+sentence = " The quick brown fox jumps over the lazy dog."
+
+print("Welcome to my program", full_name, + "!")
