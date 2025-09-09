07805a88a3da313e29ed224ba614f9627464a7ac
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-09 13:20:02 -0600

    debugged

--- notes/strings.py
+++ notes/strings.py
@@ -6,8 +6,8 @@ first_name = input("what is your name:\n").strip().upper().title()
 
 last_name = input("what is your last name:\n")
 
-full_name = first_name + last_name
+full_name = first_name + " " + last_name
 
 sentence = " The quick brown fox jumps over the lazy dog."
 
-print("Welcome to my program", full_name, + "!")
+print("Welcome to my program", full_name + "!")
