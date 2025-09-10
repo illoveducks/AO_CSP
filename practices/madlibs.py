630513823c659641bfca4157ef570e27a70a3261
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-10 13:11:04 -0600

    fixed commands

--- practices/madlibs.py
+++ practices/madlibs.py
@@ -1,7 +1,7 @@
 # AO 6th Mad libs
 
-verb = input("give me a verb:\n").strip.lower
-adjective = input("give me an adjective:\n").strip.lower
-name = input("give me a name:\n").strip.title
+verb = input("give me a verb:\n").strip().lower()
+adjective = input("give me an adjective:\n").strip().lower()
+name = input("give me a name:\n").strip().title()
 
 print("he was", verb, " with a jolly rancher,", name, "sure loves his jolly ranchers, he likes it when they're", adjective)
