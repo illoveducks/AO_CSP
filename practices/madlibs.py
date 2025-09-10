49d8812a0be184613ce524174dc8a8ed638bbb45
illoveducks <airam.ortiz@ucas-edu.net>
2025-09-10 13:39:48 -0600

    story

--- practices/madlibs.py
+++ practices/madlibs.py
@@ -3,7 +3,7 @@
 verb = input("give me a verb:\n").strip().lower()
 adjective = input("give me an adjective:\n").strip().lower()
 name = input("give me a name:\n").strip().title()
-Name = input("give me another name:\n").strip().capitalize
-candy = input("give me a candy:\n")
+name2 = input("give me another name:\n").strip().title()
+candy = input("give me a candy:\n").strip().lower()
 
-print("he was", verb, " with a jolly rancher,", name, "sure loves his jolly ranchers, he likes it when they're", adjective + "." "hey that other guy, " Name, "")
+print("he was", verb, " with a jolly rancher,", name + ".", "Sure loves his jolly ranchers, he likes it when they're", adjective + "." " Hey that other guy, ", name2, "he likes", candy, " Just how ", name, " likes jolly ranchers.")
